36c5c65715ea956c60281e63e8be9d5df2fd4170
Matthew Robinson <Matthew.m.robinson10@gmail.com>
2022-11-29 10:23:15 -0500

    First Commit

---
+++ exercises/Business-Time-Project/app.js
@@ -0,0 +1,27 @@
+const links = document.querySelectorAll(".nav-list li a");
+
+for (link of links) {
+  link.addEventListener("click", smoothScroll);
+}
+
+function smoothScroll(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+  });
+}
+{
+  const nav = document.querySelector(".nav");
+  let lastScrollY = window.scrollY;
+
+  window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+      nav.classList.add("nav--hidden");
+    } else {
+      nav.classList.remove("nav--hidden");
+    }
+
+    lastScrollY = window.scrollY;
+  });
+}
